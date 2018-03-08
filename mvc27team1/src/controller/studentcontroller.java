d12ba495368565cc39b1d5fba70ef516a87dab53
wsj8648 <wsj8648@192.168.219.107>
2018-03-08 22:44:52 +0900

    [왕서준] studentcontroller 수정

--- mvc27team1/src/controller/studentcontroller.java
+++ mvc27team1/src/controller/studentcontroller.java
@@ -16,7 +16,11 @@ public class studentcontroller extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//1.request.처리
+		//1.request.처리 
+		String id=request.getParameter("StudentId");
+		String pw=request.getParameter("StudentPw");
+		System.out.println(id+"입력된아이디");
+		
 		//2.모델(DAO) 호출
 		//3.redirect(다른 controller호출)
 	}
