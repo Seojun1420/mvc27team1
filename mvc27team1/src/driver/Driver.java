37c78c6669b3b87a76e9bfa217be8f14aabd36f0
smart-202-1 <smart-202-1@smart-202-1-PC>
2018-03-09 15:36:39 +0900

    [왕서준] driver 주석 처리

--- mvc27team1/src/driver/Driver.java
+++ mvc27team1/src/driver/Driver.java
@@ -1,5 +1,6 @@
 package driver;
 
+//드라이버에 필요한 파일을  import 해준다
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,12 +10,17 @@ public class Driver {
 		
 		Connection connection = null;
 		
+		//mysql 드라이버 로딩
 		Class.forName("com.mysql.jdbc.Driver");
 		
+		//mysql 연결을 위해 ip, port, dbid, dbpw, db명 입력
 		String jdbcDriver ="jdbc:mysql://localhost:3306/mvc?useUnicode=true&characterEncoding=utf-8";
 		String dbUser = "root";
 		String dbPass = "java0000";
-		
+	
+		/* DriverManger 클래스의 getConnection 메서드 호출한다 .
+	     * 이때  jdbcDriver, dbUser, dbpass를 매개변수 값으로 보낸다.
+	     */
 		connection = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
 	
 		return connection;
